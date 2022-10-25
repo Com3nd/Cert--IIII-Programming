831d1bde4eff84bd67ede611b734cd5768a95189
Mustafa Salman <111153100+Com3nd@users.noreply.github.com>
2022-10-25 15:36:04 +0800

    Add files via upload

---
+++ Assesment Task 2 Part A/DataService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class DataService
+    {
+        //Setting up DataServices to store all of the data
+
+        private Car[] Cars = new Car[20];
+        private List<Car> SoldCarsList = new List<Car>();
+
+        public DataService()
+        {
+            for (int i = 0; i < Cars.Length; i++)
+            {
+                Cars[i] = new Car(i + 1, "Ford", "Mustang", 2015, 50000, "Cars_Mustang");
+            }
+        }
+
+        //Get method to get the cars array
+        public Car[] GetCars()
+        {
+            return Cars;
+        }
+
+        //Private method to check if there is any empty car slot
+        private int VacancyCheck(Car[] cars)
+        {
+            for (int i = 0; i < cars.Length; i++)
+            {
+                if (cars[i] == null)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        //Method that intiates the selling of a car
+        public void SellCar(Car oldCar)
+        {
+            for (int i = 0; i < Cars.Length; i++)
+            {
+                if (oldCar == Cars[i])
+                {
+                    Cars[i] = null;
+
+                    SoldCarsList.Add(oldCar);
+                }
+            }
+        }
+
+        //Method for adding a car to inventory
+        public void AddCar(Car newCar)
+        {
+            int vacantIndex = VacancyCheck(Cars);
+
+            if (vacantIndex > -1)
+            {
+                Cars[vacantIndex] = newCar;
+            }
+        }
+
+    }
+}
---
+++ Assesment Task 2 Part A/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRUD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataService dataService = new DataService();
+            InitializeComponent();
+        }
+
+        //Method that refreshes the list box
+        void Refresh()
+        {
+            DataService dataService = new DataService();
+            ListBox_CarsInventory.ItemsSource = dataService.GetCars();
+        }
+
+
+        //Method that contains code that is activiated when a user presses the Add button
+        private void ButtonAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void ButtonRemoveCar_Click(object sender, RoutedEventArgs e)
+        {
+            DataService dataService = new DataService();
+            dataService.SellCar((Car)ListBox_CarsInventory.SelectedItem);
+            ListBox_CarsInventory.ItemsSource = dataService.GetCars();
+        }
+
+        private void ListBox_CarsInventory_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
